b01bbcf6b42f48eaa4b7943b4c0beda0ccbe76a8
Carmen Odimba <carmen.odimba@laposte.net>
2020-11-14 19:20:26 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+
+const posts = [
+    {
+        username: "Carmen",
+        title: "Post 1"
+    },
+    {
+        username: "Jane",
+        title: "Post 2"
+    }
+]
+
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name))
+    // will only return the post the user has access to
+})
+
+// Middleware to authenticate token
+function authenticateToken(req, res, next) {
+    // get token from header
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
+
+app.listen(3000)
